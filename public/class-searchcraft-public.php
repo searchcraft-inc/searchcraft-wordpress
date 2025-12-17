990fb0063b72c1b72427faf33d613dd4fb28f9e4
Don MacKinnon <dmackinnon@searchcraft.io>
2025-12-16 20:54:42 -0700

    update SDK to 0.13.2

--- public/class-searchcraft-public.php
+++ public/class-searchcraft-public.php
@@ -397,7 +397,7 @@ class Searchcraft_Public {
 
 		wp_enqueue_script(
 			$this->plugin_name . '-sdk-components',
-			plugin_dir_url( __FILE__ ) . 'sdk/components/index.js?v=0.13.1',
+			plugin_dir_url( __FILE__ ) . 'sdk/components/index.js?v=0.13.2',
 			$script_deps,
 			$this->version,
 			true
@@ -436,14 +436,14 @@ class Searchcraft_Public {
 		// Add CSS for Searchcraft components.
 		wp_enqueue_style(
 			$this->plugin_name . '-sdk-hologram-styles',
-			plugin_dir_url( __FILE__ ) . 'sdk/themes/hologram.css?v=0.13.1',
+			plugin_dir_url( __FILE__ ) . 'sdk/themes/hologram.css?v=0.13.2',
 			$style_deps,
 			$this->version,
 			'all'
 		);
 		wp_enqueue_style(
 			$this->plugin_name . '-sdk-styles',
-			plugin_dir_url( __FILE__ ) . 'css/searchcraft-sdk.css?v=0.13.1',
+			plugin_dir_url( __FILE__ ) . 'css/searchcraft-sdk.css?v=0.13.2',
 			$style_deps,
 			$this->version,
 			'all'
